68344e05cf4c839621f910ec44a7df66cb870331
mayur kyatham <97827834+mayurkyatham18@users.noreply.github.com>
2022-02-25 21:29:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,21 @@ navbar.classList.toggle('active');
 loginForm.classList.remove('active');
 searchForm.classList.remove('active');
 }
+
 let loginForm = document.querySelector('.login-form')
 document.querySelector('#login-btn').onclick = () =>{
 loginForm.classList.toggle('active');
 navbar.classList.remove('active');
 searchForm.classList.remove('active');
 }
+
+ let videoBtn = document.querySelectorAll('.vid-btn');
+ videoBtn.forEach(btn =>{
+    btn.addEventListener('click', ()=>{
+        document.querySelector('.controls .active').classList.remove('active');
+        btn.classList.add('active');
+        let src = btn.getAttribute('data-src');
+        document.querySelector('#video-slider').src = src;
+    });
+});
 
