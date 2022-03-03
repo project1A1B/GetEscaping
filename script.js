9df27ae1b68a8887c2aafee5138ff37158618295
Utsav Kuntalwad <92204155+Utsavreddy9@users.noreply.github.com>
2022-03-03 15:48:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let navbar = document.querySelector('.navbar')
 document.querySelector('#menu-btn').onclick = () =>{
   navbar.classList.toggle('active');
   loginForm.classList.remove('active');
-searchForm.classList.remove('active');
+  searchForm.classList.remove('active');
 }
 
 let loginForm = document.querySelector('.login-form')
