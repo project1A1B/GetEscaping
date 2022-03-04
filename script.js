a27637bcf8ee70a160d35f1ca2218e8846d5aa59
Utsav Kuntalwad <92204155+Utsavreddy9@users.noreply.github.com>
2022-03-04 13:16:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,10 +18,10 @@ let loginForm = document.querySelector('.login-form')
  let videoBtn = document.querySelectorAll('.vid-btn');
  videoBtn.forEach(btn =>{
     btn.addEventListener('click', ()=>{
-        document.querySelector('.controls .active').classList.remove('active');
-        btn.classList.add('active');
-        let src = btn.getAttribute('data-src');
-        document.querySelector('#video-slider').src = src;
+         document.querySelector('.controls .active').classList.remove('active');
+         btn.classList.add('active');
+         let src = btn.getAttribute('data-src');
+         document.querySelector('#video-slider').src = src;
       
     }); 
    
