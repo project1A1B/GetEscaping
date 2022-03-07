eae1f8b462a4afab64a6e6ba2ec06032887db4df
mayur kyatham <97827834+mayurkyatham18@users.noreply.github.com>
2022-03-07 21:17:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -41,5 +41,29 @@ let loginForm = document.querySelector('.login-form')
    
 });
 
+var swiper = new Swiper(".brand-slider", {
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        450: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        991: {
+          slidesPerView: 4,
+        },
+        1200: {
+          slidesPerView: 5,
+        },
+      },
+});
+
+
 
 
